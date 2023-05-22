42c0f142ac864d88b23c26f9b1f821bb60a3373e
Rayhanali-dev <rayhanalidev@gmail.com>
2023-05-22 16:08:37 +0600

    search done

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("", cors(corsConfig))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bglkkp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,7 +32,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     const database = client.db("toyDB");
     const toyCollection = database.collection("toys");
@@ -34,12 +41,26 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/toys/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await toyCollection.findOne(query);
+
+    app.get('/toys/:text', async (req, res) => {
+      const searchText = req.params.text;
+      console.log(searchText)
+      const result = await toyCollection.find({
+        $or: [
+          { name: { $regex: searchText, $options: "i" } }
+        ],
+      })
+        .toArray()
       res.send(result)
     })
+
+    app.get('/single-toy-details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    })
+
 
     app.get('/categoryToys/:category', async(req, res) => {
       const category = req.params.category;
@@ -108,6 +129,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('toy marketplace server is running');
